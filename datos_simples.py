ad80d23187a060a2a8fab25e5e0156807b4afc9d
Andrés Guerrero <andresdavid.g.r2898@gmail.com>
2025-05-08 19:06:24 +0200

    I´m studing the bases to learn Python.

---
+++ datos_simples.py
@@ -0,0 +1,16 @@
+'''Datos deñl desarrrollador:
+    Nombre: Juan Carlos
+    Fecha: 2023-10-01
+    Descripción: Este script imprime "Hello, World!" en la consola.
+'''
+"""Datos del desarrollador:
+    Nombre: Juan Carlos
+    Fecha: 2023-10-01
+    Descripción: Este script imprime "Hello, World!" en la consola.
+"""
+
+40 # valor int
+40.0 # valor float
+
+True # valor booleano
+False # valor booleano
---
+++ varaibles.py
@@ -0,0 +1,23 @@
+a = 2
+a -= 3
+a += 5
+b = 17
+c = a + b
+print(c)
+
+# "A" declara la variables y el 2 es la definición de la variable
+
+# La variable "a", puede definirse tantas veces como yo quiera, por eso es una variable
+
+nombre = "Andrés"
+apellido = "Guerrero"
+
+# Concatenar con f-strings
+Bienvenida = f"Hola {nombre} {apellido}, bienvenido a la clase de Python"
+
+# Concatenar con el operador +
+bienvenida = "Hola" + "Cómo estás"
+
+# Los operadores de pertenecia que se colocan en el console, son: in y not in
+
+print("Guerrero" in Bienvenida)
