a3197328bed010de7523d649dbb6d7065038e9fb
Andrés Guerrero <andresdavid.g.r2898@gmail.com>
2025-05-09 18:59:26 +0200

    Having learned how to create variables and manage both complex and simple dates, i am now creating an operators folder to store the lessons for my new course of python

--- datos-complejos.py
+++ datos-complejos.py
@@ -11,3 +11,19 @@ lista[3] = False
 #tupla[3] = False
 
 print(lista[2])
+
+# Creando un conjunto (set) (no se accede a elementos por indice y no almacena datos duplicados)
+conjunto = {"Andrés", "desarrollador", "Road to Data", True}
+
+# print(conjunto[2]) # Esto no es válido, ya que no se puede acceder a los elementos por índice
+
+# Creando un diccionario (dict)
+diccionario = {
+  'nombre': "Andrés",
+  'apellido': "Guerrero",
+  'edad': 27,
+  'trabajo': "desarrollador",
+  'empresa': "Road to Data",
+  'activo': True
+}
+print(diccionario['edad'] + 1) # Accediendo al valor de la clave 'empresa'
