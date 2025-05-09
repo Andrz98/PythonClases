e7dde13302ef25c57f83c612a09c3bbc51d10a67
Andrés Guerrero <andresdavid.g.r2898@gmail.com>
2025-05-09 19:50:12 +0200

    The files have been relocated in their correct folders

--- operators/op-aritmetics.py
+++ operators/op-aritmetics.py
@@ -1,3 +1,29 @@
-# Suma y resta (+ y -)
+# Plus and minus operators
+from tokenize import Exponent
 
-# Multiplicación y división (* y /)
+
+plus = 3 + 5
+minus = 9 - 2
+print("Plus:", plus)
+print("Minus:", minus)
+
+# Multiplication and división operators
+multiplication = 4 * 7
+division = 8 / 2
+print("Multiplication:", multiplication)
+print("Division:", division) # When you use the division in Python. The result will always be a float and never an int.
+
+# Exponentiation (**)
+exponent = 12 ** 5
+print("Exponent:", exponent)
+
+# Floor division (//)
+floor_division = 12 // 5 # When you use the floor division in Python. The result will always be an int. The floor division rounds down (redonde hacía abajo).
+
+#type function
+type_of_date = type(floor_division)
+print("Type of date is an:", type_of_date)
+
+# modulo
+modulo = 14 % 3
+print("Modulo:", modulo)
