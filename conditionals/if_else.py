1c34646ee45a2ae9a29f4ad99b3601b6270a06e5
Andrés Guerrero <andresdavid.g.r2898@gmail.com>
2025-05-09 21:36:37 +0200

    Im using the if_else conditional

---
+++ conditionals/if_else.py
@@ -0,0 +1,13 @@
+# if condition:
+#     action
+
+age = 17
+if age > 18:
+    print("Puedes pasar sinvergüenza")
+elif age < 18:
+        print("No puedes entrar bandido")
+elif age > 18:
+        print("No puedes entrar bandido")
+
+
+print("Si puedieras pasar, entonces se entrar con bebida incluida")
