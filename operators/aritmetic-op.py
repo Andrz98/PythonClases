9ef1cc25f348b59309b2e168fe644a12e38788a0
Andrés Guerrero <andresdavid.g.r2898@gmail.com>
2025-05-12 18:09:15 +0200

    I´m learning how to use the new extensions to make good code in my visual

--- operators/aritmetic-op.py
+++ operators/aritmetic-op.py
@@ -1,7 +1,4 @@
 # Plus and minus operators
-from tokenize import Exponent
-
-
 plus = 3 + 5
 minus = 9 - 2
 print("Plus:", plus)
@@ -11,16 +8,20 @@ print("Minus:", minus)
 multiplication = 4 * 7
 division = 8 / 2
 print("Multiplication:", multiplication)
-print("Division:", division) # When you use the division in Python. The result will always be a float and never an int.
+# When you use the division in Python.
+# The result will always be a float and never an int.
+print("Division:", division)
 
 # Exponentiation (**)
 exponent = 12 ** 5
 print("Exponent:", exponent)
 
 # Floor division (//)
-floor_division = 12 // 5 # When you use the floor division in Python. The result will always be an int. The floor division rounds down (redonde hacía abajo).
+# When you use the floor division in Python. The result will always be an int.
+# The floor division rounds down (redonde hacía abajo).
+floor_division = 12 // 5
 
-#type function
+# type function
 type_of_date = type(floor_division)
 print("Type of date is an:", type_of_date)
 
